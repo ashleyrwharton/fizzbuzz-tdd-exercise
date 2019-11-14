e7e0aaad97749c3b452d31fb87d478cad788e9ee
Ashley Wharton <ashley.wharton@gousto.co.uk>
2019-11-14 09:09:03 +0000

    Initial commit of all files

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+/**
+ * Fizzbuzz
+ *
+ * Create a function that iterates from 1 to n, and returns an array with an element for each number in the sequence 1 to n
+ * but with multiples of 3 replaced with "fizz", multiples of 5 replaced with "buzz", and multiples of 3 and 5 replaced 
+ * with "fizzbuzz".
+ * 
+ * fizzbuzz(18); 	// should return [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16, 17, 'fizz']
+ */
+
+function fizzbuzz (n) {
+	return 
+}
+
+module.exports = fizzbuzz
---
+++ test/test.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const expect = require('expect.js')
+const fizzbuzz = require('../fizzbuzz')
+
+describe('fizzbuzz', function () {
+
+})
